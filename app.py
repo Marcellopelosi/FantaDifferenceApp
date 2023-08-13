9acb0beaadbe6d26b809d4a421b5a0c3f55d0471
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-08-13 11:59:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 import streamlit as st
 import pandas as pd
 
-# Load your DataFrames here (replace with actual file paths or data loading methods)
-df1 = pd.read_csv('path_to_df1.csv')
-df2 = pd.read_csv('path_to_df2.csv')
-
-# Calculate the difference between DataFrames
-df_diff = df1.merge(df2, how='outer', indicator=True).loc[lambda x : x['_merge']!='both']
-
 # Define the Streamlit app
 def main():
-    st.title("Fanta app: differenza tra listoni")
-    
-    # Display the original DataFrames
-    st.subheader("listone vecchio:")
-    st.write(df1)
+    st.title("DataFrame Difference Viewer")
 
-    st.subheader("listone nuovo:")
-    st.write(df2)
+    # Upload the CSV files
+    st.sidebar.header("Upload DataFrames")
+    uploaded_file1 = st.sidebar.file_uploader("Upload CSV file for DataFrame 1", type=["csv"])
+    uploaded_file2 = st.sidebar.file_uploader("Upload CSV file for DataFrame 2", type=["csv"])
 
-    # Display the differences
-    st.subheader("Differenze:")
-    st.write(df_diff)
+    if uploaded_file1 and uploaded_file2:
+        # Load DataFrames from uploaded files
+        df1 = pd.read_csv(uploaded_file1)
+        df2 = pd.read_csv(uploaded_file2)
+
+        # Calculate the difference between DataFrames
+        df_diff = df1.merge(df2, how='outer', indicator=True).loc[lambda x: x['_merge'] != 'both']
+
+        # Display the original DataFrames
+        st.subheader("DataFrame 1:")
+        st.write(df1)
+
+        st.subheader("DataFrame 2:")
+        st.write(df2)
+
+        # Display the differences
+        st.subheader("Differences:")
+        st.write(df_diff)
 
 if __name__ == '__main__':
     main()
